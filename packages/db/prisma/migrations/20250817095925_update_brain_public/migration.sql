93e84880814c18df94beaf612a60924e4694e71d
Saksham Khare <sakshamkhare19@gmail.com>
2025-08-17 21:57:12 +0530

    feat: db migration

---
+++ packages/db/prisma/migrations/20250817095925_update_brain_public/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPublic` on the `brains` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."brains" DROP COLUMN "isPublic";
